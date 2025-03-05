deab2b6db8e6171ff40f9518b9df0823b556236c
nikita122333 <131138187+nikita122333@users.noreply.github.com>
2025-03-05 16:53:58 +0300

    task2.py

--- task2.py
+++ task2.py
@@ -1,11 +1,8 @@
-def task2(a,b):
-    if a>b:
-        c = a
-        while c%b != 0:
-            c +=a
-    else:
-        c = b
-        while c%a != 0:
-            c +=b
+def task2(com1,com2):
+    c = 0
+    while c%com1 != 0 or c%com2!=0 or c ==0:
+        if com1 > com2:
+            c +=com1
+        else:
+            c +=com2
     return c
-task2(7,5)
